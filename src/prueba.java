9e5841226c7af28255703c5c2a812660c38d4fd7
Leslie SanV <111658258+paolasanv@users.noreply.github.com>
2023-10-08 11:57:45 -0600

    Update prueba.java

--- src/prueba.java
+++ src/prueba.java
@@ -8,6 +8,21 @@ public class prueba{
 		Articulo d = new DescuentoTreinta(pan);
 		carrito.agregarArticulo(d);
 		System.out.println(carrito.informacion());
+
+		
+	//**Prueba de crear una cuenta cheemsmart y acceder a los datos*
+		Persona p = new Persona("Persona1", "direccion", 55742587);
+		CuentaBancaria banco = new CuentaBancaria(p, 123456, 12.0);
+		CuentaCheemsMart c = new CuentaCheemsMart(p, banco, "user00", "contra123", "mexico");
+		
+		System.out.println(c.nombre());
+		System.out.println(c.direccion());
+		System.out.println(c.telefono());
+		System.out.println(c.pais());
+		System.out.println(c.usuario());
+		System.out.println(c.contrasenia());
+		System.out.println(c.numeroCuenta());
+		System.out.println(c.saldo());
 		
 		
 	}
