b7f6668417e6f62541b9ef5e7bf2ebea535b8d7c
Leslie SanV <111658258+paolasanv@users.noreply.github.com>
2023-10-09 21:01:47 -0600

    Update prueba.java

--- src/prueba.java
+++ src/prueba.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class prueba{
 	
 	public static void main(String[] args){
-	//**Prueba de decorator y composite**
+		boolean seguir=false;
+		int opcion=0;
+		int contador =0;
+		Scanner sc = new Scanner(System.in);
+	
+		//Prueba de decorator y composite**
 		CarritoVirtual carrito = new CarritoVirtual();
 		Articulo pan = new Pan();
 		carrito.agregarArticulo(pan);
@@ -9,12 +15,12 @@ public class prueba{
 		carrito.agregarArticulo(d);
 		System.out.println(carrito.informacion());
 
-		
-	//**Prueba de crear una cuenta cheemsmart y acceder a los datos*
+		//Prueba de crear una cuenta cheemsmart y acceder a los datos*
 		Persona p = new Persona("Persona1", "direccion", 55742587);
 		CuentaBancaria banco = new CuentaBancaria(p, 123456, 12.0);
 		CuentaCheemsMart c = new CuentaCheemsMart(banco, "user00", "contra123", "mexico");
 		
+		//c.hacerCompra(10.0);
 		System.out.println(c.nombre());
 		System.out.println(c.direccion());
 		System.out.println(c.telefono());
@@ -25,5 +31,67 @@ public class prueba{
 		System.out.println(c.saldo());
 		
 		
+		CheemsMart tienda = new CheemsMart();
+		tienda.agregarCliente(c);
+		
+		do{
+		System.out.println("");
+		System.out.println("-*-*-*-*-* T I E N D A  C H E E M S M A R T -*-*-*-*-* ");
+		System.out.println("");
+		System.out.println("		1. Iniciar Sesion");
+		System.out.println("		0. Salir de la tienda");
+		System.out.println("");
+		System.out.print("Seleccione la opción que desee ejecutar: ");
+			while(true){
+				try {
+					String opcionUsuario = sc.nextLine();
+					opcion =  Integer.parseInt(opcionUsuario);
+					if(opcion >-1 && opcion < 2){ 
+						break;
+					}else{ System.out.println("		Por favor, elige una opción válida");
+					}
+				}catch (NumberFormatException ex){
+					System.out.println("		Por favor, elige una opción válida");
+				}
+			}
+				switch(opcion){
+					case 1:
+						System.out.println("");
+						System.out.println("---- I N I C I A R   S E S I Ó N -----");
+						System.out.println("");
+						while(!tienda.existeUsuario()){
+								try{
+								tienda.iniciarSesion();
+								}catch(UnsupportedOperationException ex){
+									System.out.println("	¡Aún no hay usuarios registrados en el sistema!");
+									seguir=true;
+									break;
+								}
+							contador++;
+							if(contador>3){
+								System.out.println("");
+								System.out.println("¡Se ha activado el sistema de inicio de sesión seguro!");
+								System.out.println("	Excedió la cantidad de intentos permitidos para iniciar sesión");
+								System.out.println("		Por seguridad, será redirigido a la pantalla principal");
+								System.out.println("");
+								seguir=true;
+								break;
+							}
+						}
+						
+						
+						
+						if(tienda.existeUsuario()){
+							tienda.asignarIdioma();
+							tienda.entrarAlaTienda();
+							seguir = true;
+						}
+						
+						
+						break;
+					case 0:
+						System.exit(0);	
+				}
+		}while(seguir);
 	}
 }
