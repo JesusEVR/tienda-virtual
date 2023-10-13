a4f7753da9754b937a9577ad7831edb3352f3ac6
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-10-12 19:21:21 -0600

    Create Television.java

---
+++ src/Television.java
@@ -0,0 +1,40 @@
+/**
+ * Clase que define la información del articulo Television
+ *
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ * @version octubre 2023
+ * @see Articulo
+ *
+ */
+public class Television implements Articulo{
+  
+  /**
+   * Constructor por omision del articulo Television
+   *
+   */
+	public Television(){
+	}
+
+	/**
+	 * Devuelve el precio del artículo
+	 */
+	public double precio(){
+		return 7999;
+	}
+	
+	/**
+	 * Devuelve una cadena con la información del artículo
+	 */
+	public String informacion(){
+		return "Código: " +codigoBarras()+ " Artículo: Television de 55 pulgadas"+"\n"+ "	Precio: " +precio();
+	}
+
+	/**
+	 * Devuelve el codigo de barras del artículo
+	 */
+	public int codigoBarras(){
+		return 1100;
+	}
+}
