d6afbaef8ec0ee7b3ef365ce235f7b900aa1d451
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-10-12 19:11:56 -0600

    Documentacion

--- src/Pan.java
+++ src/Pan.java
@@ -1,24 +1,39 @@
 /**
-* Clase que define la información del artículo pan
-*
-* @author paolasanv
-* @author Supr-Lilito
-* @author JesusEVR
-* @version octubre 2023
-* @see Articulo
-*
-*/
+ * Clase que define la información del articulo Pan
+ *
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ * @version octubre 2023
+ * @see Articulo
+ *
+ */
 public class Pan implements Articulo{
-	
+
+	/**
+         * Constructor por omision del articulo Pan
+         *
+         */
 	public Pan(){
 	}
-	
+
+	/**
+	 * Devuelve el precio del artículo
+	 */
 	public double precio(){
 		return 25;
 	}
+	
+	/**
+	 * Devuelve una cadena con la información del artículo
+	 */
 	public String informacion(){
 		return "Código: " +codigoBarras()+ " Artículo: Pan de caja integral"+"\n"+ "	Precio: " +precio();
 	}
+
+	/**
+	 * Devuelve el codigo de barras del artículo
+	 */
 	public int codigoBarras(){
 		return 1000;
 	}
