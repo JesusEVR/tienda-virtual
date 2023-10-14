0392ca327ac5203f38b568d4675144bf4f543038
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-10-14 14:20:06 -0600

    Documentacion

--- src/DescuentoTreinta.java
+++ src/DescuentoTreinta.java
@@ -1,28 +1,44 @@
 /**
-* Clase abstracta que permite aplicar un treinta porciento 
-* de descuento a un artículo
-*
-* @author paolasanv
-* @author Supr-Lilito
-* @author JesusEVR
-* @version octubre 2023
-* @see Articulo
-* @see Descuento
-*
-*/
+ * Clase abstracta que permite aplicar un treinta porciento 
+ * de descuento a un artículo
+ *
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ * @version octubre 2023
+ * @see Articulo
+ * @see Descuento
+ *
+ */
 public class DescuentoTreinta extends Descuento{
-	
+
+	/**
+         * Constructor por parametros
+         *
+	 * @param centro El articulo que recibira un descuento en su precio
+         */
 	public DescuentoTreinta(Articulo centro){
 		super(centro);
 	}
-	
+
+	/**
+	 * Devuelve el precio del artículo con el descuento aplicado
+	 */
 	public double precio(){
 		double nuevoPrecio = 0.7 * centro.precio();
 		return nuevoPrecio;
 	}
+
+	/**
+	 * Devuelve una cadena con la información del articulo
+	 */
 	public String informacion(){
 		return centro.informacion() + " -- Precio con 30% de descuento: $" +precio()+" MXN";
 	}
+
+	/**
+	 * Devuelve el codigo de barras del articulo
+	 */
 	public String codigoBarras(){
 		return centro.codigoBarras();
 	}
