10edfd1b687c3922078533868a413add5ee42fbf
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-10-14 13:59:41 -0600

    Documentacion

--- src/Descuento.java
+++ src/Descuento.java
@@ -1,29 +1,45 @@
 /**
-* Clase abstracta que permite aplicar descuentos
-*
-* @author paolasanv
-* @author Supr-Lilito
-* @author JesusEVR
-* @version octubre 2023
-* @see Articulo
-*
-*/
+ * Clase abstracta que permite aplicar descuentos a los productos de la tienda CheemsMart.
+ *
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ * @version octubre 2023
+ * @see Articulo
+ *
+ */
 public abstract class Descuento implements Articulo{
 	/**
-	* Artículo al que se le aplicará el descuento
-	*/
+	 * Artículo al que se le aplicara el descuento
+	 */
 	protected Articulo centro;
-	
-	public Descuento(Articulo centro){
+
+	/**
+         * Constructor por parametros
+         *
+	 * @param centro El articulo que recibira un descuento en su precio
+         */
+	public Descuento (Articulo centro){
 		this.centro = centro;
 	}
-	
+
+	/**
+	 * Devuelve el precio del artículo
+	 */
 	public double precio(){
 		return centro.precio();
 	}
+
+	/**
+	 * Devuelve una cadena con la información del articulo
+	 */
 	public String informacion(){
 		return centro.informacion();
 	}
+	
+	/**
+	 * Devuelve el codigo de barras del articulo
+	 */
 	public String codigoBarras(){
 		return centro.codigoBarras();
 	}
