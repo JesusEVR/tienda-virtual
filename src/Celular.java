21719e0fb2c40b567083f77c1c1008b7f3d0a6e6
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-10-14 20:08:42 -0600

    Create Celular.java

---
+++ src/Celular.java
@@ -0,0 +1,40 @@
+/**
+ * Clase que define la información del articulo Celular
+ *
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ * @version octubre 2023
+ * @see Articulo
+ *
+ */
+public class Celular implements Articulo{
+	
+	/**
+         * Constructor por omision del articulo Celular
+         *
+         */
+	public Celular(){
+	}
+
+	/**
+	 * Devuelve el precio del artículo
+	 */
+	public double precio(){
+		return 26999;
+	}
+	
+	/**
+	 * Devuelve una cadena con la información del artículo
+	 */
+	public String informacion(){
+		return "Código: " +codigoBarras()+ " Artículo: Celular AyFound 18, 252 GB de almacenamiento y 3 horas de bateria"+"\n"+ "	Precio: $" +precio() +" MXN";
+	}
+
+	/**
+	 * Devuelve el codigo de barras del artículo
+	 */
+	public String codigoBarras(){
+		return "ET204";
+	}
+}
