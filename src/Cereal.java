4921826f06bbe9a095d224ab1dac3e1cdb7aa4ae
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-10-14 20:10:58 -0600

    Create Cereal.java

---
+++ src/Cereal.java
@@ -0,0 +1,40 @@
+/**
+ * Clase que define la información del articulo Cereal
+ *
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ * @version octubre 2023
+ * @see Articulo
+ *
+ */
+public class Cereal implements Articulo{
+
+	/**
+         * Constructor por omision del articulo cereal
+         *
+         */
+	public Cereal(){
+	}
+
+	/**
+	 * Devuelve el precio del artículo
+	 */
+	public double precio(){
+		return 57;
+	}
+	
+	/**
+	 * Devuelve una cadena con la información del artículo
+	 */
+	public String informacion(){
+		return "Código: " +codigoBarras()+ " Artículo: Caja de cereal CochoPiscris"+"\n"+ "	Precio: $" +precio() + " MXN";
+	}
+
+	/**
+	 * Devuelve el codigo de barras del artículo
+	 */
+	public String codigoBarras(){
+		return "AL101";
+	}
+}
