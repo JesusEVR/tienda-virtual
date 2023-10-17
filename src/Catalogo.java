ee1ff3f651758579f37f195d239869fdf34589d8
Supr-Lilito <124287410+Supr-Lilito@users.noreply.github.com>
2023-10-16 20:58:04 -0600

    catalogo :v

---
+++ src/Catalogo.java
@@ -0,0 +1,17 @@
+import java.util.Iterator;
+/**
+* Interfaz que define la característica común de los catálogos
+*
+*@author paolasanv
+*@author Supr-Lilito
+*@author JesusEVR
+*
+*/
+public interface Catalogo{
+	
+	/**
+	* Crea un iterador sobre el catálogo
+	*
+	*/
+	public Iterator creaIterador();
+}
