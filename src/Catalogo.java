d5a7c1c2d08c5a9d0ca0f701b5c9500a803a903c
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-10-17 20:13:32 -0600

    Update Catalogo.java

--- src/Catalogo.java
+++ src/Catalogo.java
@@ -1,17 +1,16 @@
 import java.util.Iterator;
 /**
-* Interfaz que define la característica común de los catálogos
-*
-*@author paolasanv
-*@author Supr-Lilito
-*@author JesusEVR
-*
-*/
+ * Interfaz que define la característica común de los catálogos
+ *
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ */
 public interface Catalogo{
 	
 	/**
-	* Crea un iterador sobre el catálogo
-	*
-	*/
+	 * Crea un iterador sobre el catálogo
+	 *
+	 */
 	public Iterator creaIterador();
 }
