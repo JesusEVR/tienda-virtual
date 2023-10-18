b6d8772a579c9efabf55895b21c5153d331bfb60
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-10-17 20:21:46 -0600

    Update CatalogoElectrodomestico.java

--- src/CatalogoElectrodomestico.java
+++ src/CatalogoElectrodomestico.java
@@ -7,6 +7,7 @@ import java.util.Iterator;
  * @author paolasanv
  * @author Supr-Lilito
  * @author JesusEVR
+ * @version octubre 2023
  *
  */
 public class CatalogoElectrodomestico implements Catalogo{
