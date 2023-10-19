96e9d6746516968f6ca95bd7d1378d58cb7081e3
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-10-19 10:14:54 -0600

    Update Aspiradora.java

--- src/Aspiradora.java
+++ src/Aspiradora.java
@@ -28,7 +28,7 @@ public class Aspiradora implements Articulo{
 	 * Devuelve una cadena con la información del artículo
 	 */
 	public String informacion(){
-		return "Código: " +codigoBarras()+ " Artículo: Aspiradora silenciosa de gran potencia"+"\n"+ "	Precio: $" +precio() +" MXN";
+		return "Código: " +codigoBarras()+ " - Artículo: Aspiradora silenciosa de gran potencia"+"\n"+ "	Precio: $" +precio() +" MXN";
 	}
 
 	/**
