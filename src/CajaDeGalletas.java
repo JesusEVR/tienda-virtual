af2aad37a8c6a8dd1ededa396a0edb987bc795fd
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-10-19 10:15:11 -0600

    Update CajaDeGalletas.java

--- src/CajaDeGalletas.java
+++ src/CajaDeGalletas.java
@@ -28,7 +28,7 @@ public class CajaDeGalletas implements Articulo{
 	 * Devuelve una cadena con la información del artículo
 	 */
 	public String informacion(){
-		return "Código: " +codigoBarras()+ " Artículo: Caja de galletas de chocolate"+"\n"+ "	Precio: $" +precio() + " MXN";
+		return "Código: " +codigoBarras()+ " - Artículo: Caja de galletas de chocolate"+"\n"+ "	Precio: $" +precio() + " MXN";
 	}
 
 	/**
