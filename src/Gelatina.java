c8b8de3873a7b3f916555d212ff2e2589025179e
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-10-19 10:17:41 -0600

    Update Gelatina.java

--- src/Gelatina.java
+++ src/Gelatina.java
@@ -28,7 +28,7 @@ public class Gelatina implements Articulo{
 	 * Devuelve una cadena con la información del artículo
 	 */
 	public String informacion(){
-		return "Código: " +codigoBarras()+ " Artículo: Paquete de 4 gelatinas"+"\n"+ "	Precio: $" +precio() + " MXN";
+		return "Código: " +codigoBarras()+ " - Artículo: Paquete de 4 gelatinas"+"\n"+ "   Precio: $" +precio() + " MXN";
 	}
 
 	/**
